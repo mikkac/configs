8e3ba928926d089d4ef17bbfcc8dbe4d1c221121
Mikolaj Kaczmarek <mikolaj.kaczmarek@tieto.com>
2018-04-25 12:25:47 +0200

    gen1 ;

--- new/exc_03_generator/build/tests/CMakeFiles/generatorTests-format.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/generatorTests-format"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/generatorTests-format.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- new/exc_03_generator/build/tests/CMakeFiles/generatorTests-tidy.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/generatorTests-tidy"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/generatorTests-tidy.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
