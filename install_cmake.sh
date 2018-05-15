2e8a33a428623451252019774ea741e852e83df2
Mikolaj Kaczmarek <mikolaj.kaczmarek@tieto.com>
2018-05-15 22:42:19 +0200

    add installation files for clang-6.0, cmake-3.11 and g++-7.4

---
+++ install_cmake.sh
@@ -0,0 +1,16 @@
+sudo apt remove cmake
+sudo apt purge --auto-remove cmake
+
+version=3.11
+build=1
+mkdir ~/temp
+cd ~/temp
+wget https://cmake.org/files/v$version/cmake-$version.$build.tar.gz
+tar -xzvf cmake-$version.$build.tar.gz
+cd cmake-$version.$build/
+
+./bootstrap
+make -j4
+sudo make install
+
+cmake --version
